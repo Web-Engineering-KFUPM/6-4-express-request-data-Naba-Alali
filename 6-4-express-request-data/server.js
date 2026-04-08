e69d57816e328bc681b6d9247485b50c55885916
Naba-Alali <S202268320@kfupm.edu.sa>
2026-04-08 11:48:15 +0300

    Todo5

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -111,7 +111,7 @@ import express from "express";
 // create express app instance to create web server
 
 const app = express();
-app.listen(3000, () => console.log("API running at http://localhost:3000"));
+
 
 // Query params: /echo?name=Ali&age=22
 app.get("/echo", (req, res) => {
@@ -146,8 +146,12 @@ app.param("userId", (req, res, next, userId) => {
 
 
 // Route params: /users/:userId route
+app.get("/users/:userId", (req, res) => {
+  res.json({ ok: true, userId: req.userIdNum });
+});
 
 
 // Start the server by listening
+app.listen(3000, () => console.log("API running at http://localhost:3000"));
 
 
